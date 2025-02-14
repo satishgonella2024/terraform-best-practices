3a08fcb9148b5d248f124945524ad076b5500a56
Subrahmanya Gonella <164250924+satishgonella2024@users.noreply.github.com>
2025-02-14 19:56:15 +0000

    feat: configure development environment

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,18 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   backend "s3" {
     bucket         = "terraform-state-bucket-eg"
     key            = "dev/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "terraform-state-locks"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,17 @@
+# environments/dev/main.tf
 module "vpc" {
-  source      = "../../modules/vpc"
+  source = "../../modules/vpc"
+
   cidr_block  = "10.0.0.0/16"
   name        = "dev-vpc"
+  environment = "dev"
+
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones   = ["eu-west-2a", "eu-west-2b"]
+
+  tags = {
+    ManagedBy = "Terraform"
+    Project   = "Infrastructure"
+  }
 }
